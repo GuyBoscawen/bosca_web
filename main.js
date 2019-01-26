224a638eaf5cf4e4cd2c474871d5a5a68c20358e
GuyBoscawen <45629998+GuyBoscawen@users.noreply.github.com>
2019-01-26 00:29:46 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+	$('#btn').on('click', () => {
+		$('#contain').toggle()
+	})
+	$('#bt').on('click', () => {
+		$('#maintain').toggle()
+	})
+	$('#b').on('click', () => {
+		$('#obtain').toggle()
+	})
+	$('#bttn').on('click', () => {
+		$('#retain').toggle()
+	})
+	
+	
+})
